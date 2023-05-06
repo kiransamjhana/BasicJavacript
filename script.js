f9ad896db69fcc9e56986d9231c3dac6a7b81f89
kiransamjhana <kiranmanandhar956@gmail.com>
2023-05-06 15:27:31 +1000

    origin

---
+++ script.js
@@ -0,0 +1,163 @@
+// console.log("HELLOW WORLD");
+
+//VARIBALE
+
+//var, let,
+
+// var name = "Sam Smith";
+// let age = 44;
+// console.log(name, age);
+
+// name = "kiran manandhar";
+// age = 20;
+// const dob = "18 - 01 - 1994";
+// console.log(name, age, dob);
+// console.log(old);
+// const
+
+//Js-Operators
+
+// let a = 5;
+// let b = 6;
+
+// const ans = a * b;
+// const ans = a ** b;
+// const ans = a / b;
+// a++; increase value
+// ++a; // increase value first
+// a--;
+
+// console.log(--a);
+
+// console.log(a);
+
+// Comparision
+
+// const v1 = 5;
+// const v2 = "5";
+
+// const result = v1 <= v2;
+// console.log(result);
+
+// Data type
+
+// const dt = undefined;
+
+// const dt = null; // null shows object as data type
+
+// console.log(typeof dt);
+
+// const name1= "kiran"
+// console.log (name1)
+// const n
+
+// Reference type
+
+// const obj = {
+//   name: "kiran",
+// };
+
+// console.log(obj);
+
+//Math
+const person = [(name = "kiran")];
+
+// console.log(Math.PI);
+
+//function
+
+// function addTwoNumbers(num1, num2) {
+//   //   let a = 2;
+//   //   let b = 3;
+
+//   //   console.log(a + b);
+
+//   console.log(num1 + num2);
+// }
+
+// addTwoNumbers(12, 5);
+
+//  Math .round
+
+// // console.log(Math.round(5.5));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(64));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(7.9));
+
+// console.log(Math.min(1, 2, 3, 9, 10));
+// console.log(Math.max(2, 3));
+
+// console.log(Math.round(Math.random() * 10));
+
+// Js _Conditional Statements
+
+// let a = 4;
+
+// if (a == 5) {
+//   console.log(" i am if statement");
+// } else if (a == 4) {
+//   console.log(" i am else if statement");
+//   // .....
+// } else {
+//   console.log(" i am outside this ");
+// }
+
+///Switch case  satement
+
+// switch ("case-2") {
+//   case "case-1":
+//     console.log("Iam insid e the case 1");
+//     break;
+
+//   case "case-2":
+//     console.log("Iam insid e the case 1");
+//     break;
+
+//   default:
+//     console.log("Iam  inside the defualt ");
+// }
+
+// Error Handling
+
+// try {
+//   const x = 5;
+
+//   console.log(x);
+// } catch (error) {
+//   console.log(error.message);
+// } finally {
+//   console.log("I am very happy");
+// }
+
+// JS- Scheduling
+
+// Set setTimeout
+
+// function hello() {
+//   console.log("kirna ");
+// }
+
+// setTimeout(hello, 5000);
+
+// function hello() {
+//   console.log("kirna ");
+// }
+
+// let output = setTimeout
+
+// setTimeout(hello, 5000);
+
+// let i = 0;
+// const interval = setInterval(() => {
+//   console.log(" Insidd ", i++);
+
+//   if (i == 10) {
+//     clearInterval(interval);
+// //   }
+// }, 1000);
+// Arrow function
+
+const addTwoNumbers = (num1, num2) => num1 + num2;
+
+console.log(addTwoNumbers(10, 2));
