c8ab3f5c0b7aa54b1a82225befd7586b266b7883
kiransamjhana <kiranmanandhar956@gmail.com>
2023-05-07 14:51:40 +1000

    new updat

--- script.js
+++ script.js
@@ -1,163 +1,434 @@
-// console.log("HELLOW WORLD");
+// // // console.log("HELLOW WORLD");
 
-//VARIBALE
+// // //VARIBALE
 
-//var, let,
+// // //var, let,
 
-// var name = "Sam Smith";
-// let age = 44;
-// console.log(name, age);
+// // // var name = "Sam Smith";
+// // // let age = 44;
+// // // console.log(name, age);
 
-// name = "kiran manandhar";
-// age = 20;
-// const dob = "18 - 01 - 1994";
-// console.log(name, age, dob);
-// console.log(old);
-// const
+// // // name = "kiran manandhar";
+// // // age = 20;
+// // // const dob = "18 - 01 - 1994";
+// // // console.log(name, age, dob);
+// // // console.log(old);
+// // // const
 
-//Js-Operators
+// // //Js-Operators
 
-// let a = 5;
-// let b = 6;
+// // // let a = 5;
+// // // let b = 6;
 
-// const ans = a * b;
-// const ans = a ** b;
-// const ans = a / b;
-// a++; increase value
-// ++a; // increase value first
-// a--;
+// // // const ans = a * b;
+// // // const ans = a ** b;
+// // // const ans = a / b;
+// // // a++; increase value
+// // // ++a; // increase value first
+// // // a--;
 
-// console.log(--a);
+// // // console.log(--a);
 
-// console.log(a);
+// // // console.log(a);
 
-// Comparision
+// // // Comparision
 
-// const v1 = 5;
-// const v2 = "5";
+// // // const v1 = 5;
+// // // const v2 = "5";
 
-// const result = v1 <= v2;
-// console.log(result);
+// // // const result = v1 <= v2;
+// // // console.log(result);
 
-// Data type
+// // // Data type
 
-// const dt = undefined;
+// // // const dt = undefined;
 
-// const dt = null; // null shows object as data type
+// // // const dt = null; // null shows object as data type
 
-// console.log(typeof dt);
+// // // console.log(typeof dt);
 
-// const name1= "kiran"
-// console.log (name1)
-// const n
+// // // const name1= "kiran"
+// // // console.log (name1)
+// // // const n
 
-// Reference type
+// // // Reference type
 
-// const obj = {
-//   name: "kiran",
+// // // const obj = {
+// // //   name: "kiran",
+// // // };
+
+// // // console.log(obj);
+
+// // //Math
+// // const person = [(name = "kiran")];
+
+// // // console.log(Math.PI);
+
+// // //function
+
+// // // function addTwoNumbers(num1, num2) {
+// // //   //   let a = 2;
+// // //   //   let b = 3;
+
+// // //   //   console.log(a + b);
+
+// // //   console.log(num1 + num2);
+// // // }
+
+// // // addTwoNumbers(12, 5);
+
+// // //  Math .round
+
+// // // // console.log(Math.round(5.5));
+// // // console.log(Math.pow(2, 3));
+// // // console.log(Math.sqrt(64));
+// // // console.log(Math.ceil(4.3));
+// // // console.log(Math.floor(7.9));
+
+// // // console.log(Math.min(1, 2, 3, 9, 10));
+// // // console.log(Math.max(2, 3));
+
+// // // console.log(Math.round(Math.random() * 10));
+
+// // // Js _Conditional Statements
+
+// // // let a = 4;
+
+// // // if (a == 5) {
+// // //   console.log(" i am if statement");
+// // // } else if (a == 4) {
+// // //   console.log(" i am else if statement");
+// // //   // .....
+// // // } else {
+// // //   console.log(" i am outside this ");
+// // // }
+
+// // ///Switch case  satement
+
+// // // switch ("case-2") {
+// // //   case "case-1":
+// // //     console.log("Iam insid e the case 1");
+// // //     break;
+
+// // //   case "case-2":
+// // //     console.log("Iam insid e the case 1");
+// // //     break;
+
+// // //   default:
+// // //     console.log("Iam  inside the defualt ");
+// // // }
+
+// // // Error Handling
+
+// // // try {
+// // //   const x = 5;
+
+// // //   console.log(x);
+// // // } catch (error) {
+// // //   console.log(error.message);
+// // // } finally {
+// // //   console.log("I am very happy");
+// // // }
+
+// // // JS- Scheduling
+
+// // // Set setTimeout
+
+// // // function hello() {
+// // //   console.log("kirna ");
+// // // }
+
+// // // setTimeout(hello, 5000);
+
+// // // function hello() {
+// // //   console.log("kirna ");
+// // // }
+
+// // // let output = setTimeout
+
+// // // setTimeout(hello, 5000);
+
+// // // let i = 0;
+// // // const interval = setInterval(() => {
+// // //   console.log(" Insidd ", i++);
+
+// // //   if (i == 10) {
+// // //     clearInterval(interval);
+// // // //   }
+// // // }, 1000);
+// // // Arrow function
+
+// // // const addTwoNumbers = (num1, num2) => num1 + num2;
+
+// // // console.log(addTwoNumbers(10, 2));
+
+// // // Loop
+
+// // // for (let i = 0; i <= 10; i++) {
+// // //   console.log("kiran ");
+// // // }
+
+// // // while loop check the condition first
+
+// // // let i = 1;
+
+// // // while (i < 10) {
+// // //   console.log(i);
+// // //   i++;
+// // // }
+
+// // //  Do while loop check the condition later
+// // // let i = 1;
+
+// // // do {
+// // //   console.log(i);
+// // //   i++;
+// // // } while (i < 0);
+
+// // // Js - Data manupulation
+
+// // // Number
+// // // parseInt folats
+// // // safe number  -2 ^53-1 to 2^53-1
+
+// // // const num = "23145";
+
+// // // console.log(typeof num);
+
+// // // // String manupulation
+
+// // // const str = " hi Ia m kiran, and i live in sydney, and  i will love you";
+
+// // // const val = str.length;
+
+// // // console.log(val);
+// // // const val = str[50];
+// // // console.log(val);
+
+// // // const val = str.indexOf("i");
+// // // const val = str.replace("i", "k");
+
+// // // // both indices count forom begining
+
+// // // const val = str.substring(-1);
+
+// // // // const val = str.slice(-14);
+// // // const val = str.includes("kiran");
+// // // const val = str.charAt(15);
+// // // // const val = str.charCodeAt(15);
+// // // const val = str.split(","); // convert string into array
+// // // const val = str.search("kiran");
+// // // const val = str.trim("");
+// // // const val = str.trim("");
+// // // console.log(val.length);
+// // // console.log(str.length);
+
+// // // const bio = (name, address) => {
+// // //   return ` today we willl learn about a ${name}   `;
+// // // };
+
+// // // const sam = bio("sam", "sydney");
+// // // console.log(sam);
+
+// // // =====Array
+
+// // // const fruits = ["apple", "banana", "orange", "kiwi"];
+// // // const item = fruits.pop;
+// // // console.log(val);
+// // // addd item in the arrrayd
+// // // fruits.push("watermelon");
+
+// // // Addd the item in the begining
+// // // fruits.unshift("melon");
+
+// // // Remove item from the end of the list
+
+// // // console.log(val);
+
+// // // const data = fruits.slice(1, 4);
+
+// // // Splice manupulliate the aoringal array as well
+
+// // // const data = fruits.splice(1, 2);
+// // // fruits.sort();
+// // // fruits.reverse();
+// // // // console.log(data);
+
+// // // console.log(fruits);
+// // // console.log(fruits, toString());
+// // //  convert array to string
+// // // const dt = fruits.join("/ß");
+// // // console.log(dt);
+
+// // // // Array loops ( veyr important )
+
+// // const fruits = ["apple", "banana", "orange", "kiwi"];
+
+// // // fruits.forEach((item, index) => {
+// // //   nsole.log(index, item);
+// // // });
+
+// // // const newArg = fruits.map((itm, i) => {
+// // //   console.log(i, itm);
+// // //   return "TM-" + itm.toLocaleUpperCase();
+// // // });
+// // // console.log(fruits, newArg);
+
+// // // const newArg = fruits.filter((itm) => {
+// // //   //return true, it will push itm to newArg , return false ignore the push
+// // //   //   if (itm.includes("p")) {
+// // //   //     return true;
+
+// // //   return itm.includes("p");
+// // // });
+// // // console.log(fruits, newArg);
+
+// // // const val = "kiran ";
+
+// // // //truthy values 'kkran"  222 , true . (,)
+// // // //falsy value "". 0, nan, false, undefined
+
+// // // console.log(!!val);
+
+// // // find method
+
+// // // const newArg = fruits.find((itm) => itm.includes("p"));
+// // // //   return true, it will push itm to newArg , return false ignore the push
+// // // console.log(newArg);
+
+// // // const banks = [23, 2342, 2, 24355, 56898];
+
+// // // const totalMoney = banks.reduce((subTtl, item) => {
+// // //   return subTtl + item;
+// // // });
+
+// // // console.log(totalMoney);
+
+// // // challange 1
+
+// // // 1. Create an array of 50 random number range between 1 and 100 programmatically.
+// // // 2. sort the arry in descending order
+
+// // //get the total value of the array
+// // // .4 divide original arry in to 2 arrays that contains even number or odd number only
+
+// // //.5 Remove the duplicate number from the original array
+
+// // //good lucklet randomNumbers = [];
+
+// // // Creating an array of 50 random number range between 1 and 100
+// // let odd = [];
+// // let even = [];
+// // const randomNumbers = [];
+// // for (let i = 0; i < 50; i++) {
+// //   randomNumbers.push(Math.floor(Math.random() * 100) + 1);
+// // }
+// // console.log("THIS IS RANDOM NUMBER", randomNumbers);
+
+// // //Sorting the array in descending orders
+
+// // const val = randomNumbers.sort(function (a, b) {
+// //   return b - a;
+// // });
+// // console.log("THIS IS IN DECENDING ORDER", val);
+
+// // //get the total value of the array
+
+// // const totalValue = randomNumbers.reduce((subTtl, item) => {
+// //   return subTtl + item;
+// // });
+// // console.log("THOTAL VALUE", totalValue);
+
+// // //Divide the array in two arrays that contains only even number or odd number
+// // function oddNumber() {
+// //   for (let i = 0; i < randomNumbers.length; i++) {
+// //     if (randomNumbers[i] % 2 == 0) {
+// //       even.push(randomNumbers[i]);
+// //     } else {
+// //       odd.push(randomNumbers[i]);
+// //     }
+// //   }
+// // }
+
+// // oddNumber();
+// // console.log("thiis is odd number", odd);
+// // console.log(" Hey kiran  this is even number ", even);
+
+// // // const uniqueArg =[]
+// // // randomNumbers.forEach(())
+
+// // // challange 2
+// // //1 create unique arrary of 50 number range betweeen 1 to 100 programmatically
+
+// // let uniqueArg=[]
+
+// // while (uniqueArg.length <=50){
+// //     const num =
+// // }
+
+// // =======Object Oriented Programming
+
+// const person = {
+//   name: "sam",
+//   age: 22,
+//   add: "sydney",
+//   gender: "male",
+// };
+// // const p2 = { ...person };
+// // person.name = "kiran";
+// person.add = "sydney";
+// // delete person.age;
+
+// // const val = person["name"];
+// // console.log(val);
+// // Destructuring
+
+// const { name, ...rest } = person; // destructuring
+// console.log(name, rest);
+
+// console.log(person);
+// //Arrow function
+// //Destructuring
+
+// const parent = (obj) => {
+//     return ' the father  name is ${obj.father }'
+// }
+// const bio = (obj) => {
+//   console.log(obj);
+
+//   return ` The person called ${obj.name} age ${obj.age} lives in ${obj.add}`;
+// };
+// const str = bio(person);
+// console.log(str);
+
+// //Default + rest + spread
+
+// ...= rest
+// =...spread()
+
+// const p2 = {
+//   name: "sam",
+//   age: 22,
+//   add: "sydney",
+//   gender: "male",
+// };
+// const p3 = {
+//   name: "sam",
+//   age: 22,
+
+//   gender: "male",
 // };
 
-// console.log(obj);
+// // JS _ES6
 
-//Math
-const person = [(name = "kiran")];
+const obj1 = {
+  fname: "Sam",
+  add: "syd",
+};
 
-// console.log(Math.PI);
-
-//function
-
-// function addTwoNumbers(num1, num2) {
-//   //   let a = 2;
-//   //   let b = 3;
-
-//   //   console.log(a + b);
-
-//   console.log(num1 + num2);
-// }
-
-// addTwoNumbers(12, 5);
-
-//  Math .round
-
-// // console.log(Math.round(5.5));
-// console.log(Math.pow(2, 3));
-// console.log(Math.sqrt(64));
-// console.log(Math.ceil(4.3));
-// console.log(Math.floor(7.9));
-
-// console.log(Math.min(1, 2, 3, 9, 10));
-// console.log(Math.max(2, 3));
-
-// console.log(Math.round(Math.random() * 10));
-
-// Js _Conditional Statements
-
-// let a = 4;
-
-// if (a == 5) {
-//   console.log(" i am if statement");
-// } else if (a == 4) {
-//   console.log(" i am else if statement");
-//   // .....
-// } else {
-//   console.log(" i am outside this ");
-// }
-
-///Switch case  satement
-
-// switch ("case-2") {
-//   case "case-1":
-//     console.log("Iam insid e the case 1");
-//     break;
-
-//   case "case-2":
-//     console.log("Iam insid e the case 1");
-//     break;
-
-//   default:
-//     console.log("Iam  inside the defualt ");
-// }
-
-// Error Handling
-
-// try {
-//   const x = 5;
-
-//   console.log(x);
-// } catch (error) {
-//   console.log(error.message);
-// } finally {
-//   console.log("I am very happy");
-// }
-
-// JS- Scheduling
-
-// Set setTimeout
-
-// function hello() {
-//   console.log("kirna ");
-// }
-
-// setTimeout(hello, 5000);
-
-// function hello() {
-//   console.log("kirna ");
-// }
-
-// let output = setTimeout
-
-// setTimeout(hello, 5000);
-
-// let i = 0;
-// const interval = setInterval(() => {
-//   console.log(" Insidd ", i++);
-
-//   if (i == 10) {
-//     clearInterval(interval);
-// //   }
-// }, 1000);
-// Arrow function
-
-const addTwoNumbers = (num1, num2) => num1 + num2;
-
-console.log(addTwoNumbers(10, 2));
+const obj2 = {
+  fName: "Kiran",
+  add: "nepal",
+};
+const combo = { ...obj1, ...obj2 };
+console.log(combo);
